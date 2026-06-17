a6c5cf9af14c12c8fd96f1437cfad67216ac035a
khanimb <khanimshb@code.edu.az>
2026-06-17 22:37:03 +0400

    cgfhyju

--- obj/Debug/net8.0/OnlineJobRecruitmentSystem.AssemblyInfo.cs
+++ obj/Debug/net8.0/OnlineJobRecruitmentSystem.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("OnlineJobRecruitmentSystem")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b3ee7119d02e4d3686c848692d898ea8bd0e5da3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+900ede5ff1fab4212e643ecb528f2cf7747e4560")]
 [assembly: System.Reflection.AssemblyProductAttribute("OnlineJobRecruitmentSystem")]
 [assembly: System.Reflection.AssemblyTitleAttribute("OnlineJobRecruitmentSystem")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
